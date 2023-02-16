ce040a684c251e6f521f7180755834a3fb6075ee
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2023-02-16 02:22:51 -0500

    Initialize CL_hw5

---
+++ socialgroundapp/src/components/Create.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import SendIcon from '@mui/icons-material/Send';
+import { Button } from '@mui/material';
+import { Mention, MentionsInput } from 'react-mentions';
+import { addCommentToPost, createComment, getUsers } from '../api/mock_api';
+import face from '../icons/face.png';
+
+function CreateComment(postId) {
+  // const [body, setBody] = useState('');
+  const [name] = useState('6376d6d927d24d3dd3e4d88a');
+  const navigate = useNavigate();
+  const [value, setValue] = useState('');
+  const [users, setUsers] = useState([]);
+  const postIdStr = JSON.stringify(postId);
+  const postid = postIdStr.substring(postIdStr.indexOf(':') + 2, postIdStr.length - 2);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  // change fetch (below) to axios: shouldn't have API request in
+  // small component (call API in top component instead)
+  // 1. change to axios
+  // 1. put this logic inside the activity feed page
+  // 2. pass down the function in activity feed page from post to create
+  // 3. comment rather than writing the API request inside the comment
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (value.length !== '') {
+      const commentData = await createComment(name, value);
+      const resp = await addCommentToPost(commentData._id, postid);
+      if (resp) {
+        setValue('');
+        document.getElementById('inpt').value = '';
+        navigate('/Feed');
+      }
+    }
+  };
+  const fetchUserList = async () => {
+    let i;
+    // let obj;
+    const res = await getUsers();
+    // console.log(typeof (res));
+    const userArray = [];
+    // res.forEach(item => console.log(item));
+    // console.log(res);
+    // console.log(res.data);
+    // const usersArray = [];
+    // res.data.users((item) => userArray.push({
+    //   id: item.id,
+    //   display: item.username,
+    // }));
+    for (i = 0; i < res.length; i += 1) {
+      // obj = { id: res[i].id, display: res[i].username };
+      userArray.push({ id: res[i].id, display: res[i].username });
+    // console.log(res[0].username);
+    // console.log(usersArray);
+    // console.log(res.type);
+    // setUsers(res[0]);
+    }
+    // console.log(JSON.stringify(userArray));
+    console.log(typeof (userArray));
+    setUsers(userArray);
+    console.log(userArray);
+  };
+  useEffect(() => {
+    fetchUserList()
+      .catch(console.err);
+  }, []);
+  //  fake user data for testing; seems it needs display as parameter
+  // const UserList = [
+  //   {
+  //     id: 'isssdds',
+  //     username: 'dkj ee',
+  //   },
+  //   {
+  //     id: 'dkfj',
+  //     username: 'superwomna',
+  //   },
+  // ];
+  // console.log(UserList.type);
+
+  return (
+
+    <form className="form-input">
+      <div className="smily-face">
+        <img src={face} className="happy-face-icon" alt="happy-face-icon" />
+      </div>
+      {/* <TextField
+        sx={{ marginLeft: 1, marginRight: 2 }}
+        required
+        maxRows={4}
+        variant="outlined"
+        fullWidth
+        onChange={handleChange}
+        placeholder="Type"
+        id="inpt"
+        label="Add a comment"
+        multiline
+      /> */}
+      <MentionsInput
+        className="mentions"
+        placeholder="Add a comment Here and Mention people using '@' "
+        id="inpt"
+        value={value}
+        onChange={handleChange}
+      >
+        <Mention
+          id="SuggestionList"
+          // data={UserList}
+          data={users}
+          trigger="@"
+          // style={{overflow-y: scroll;}}
+        />
+      </MentionsInput>
+      <Button
+        sx={{ marginRight: 3 }}
+        variant="contained"
+        size="small"
+        endIcon={<SendIcon />}
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Send
+      </Button>
+    </form>
+  );
+}
+
+export default CreateComment;
---
+++ socialgroundapp/src/components/profile.js
@@ -0,0 +1,149 @@
+import React, { useRef, useEffect, useState } from 'react';
+import '../css/profile.css';
+import { Link } from 'react-router-dom';
+import setting from '../icons/settings.png';
+import defaultUser from '../icons/default_user.png';
+import smallLogo from '../socialground_small.png';
+import circledPlus from '../images/circled_plus.jpg';
+import { getCurrentUser, getPosts } from '../api/mock_api';
+
+const profile = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const postNum = (!currentUser?.postCount || currentUser.postCount === undefined
+    || currentUser?.postCount === null) ? 0 : currentUser?.postCount;
+  const followingCount = (!currentUser?.following?.length
+      || currentUser?.following?.length === undefined
+      || currentUser?.following?.length === null) ? 0 : currentUser?.following?.length;
+  const followerCount = (!currentUser?.follower?.length
+    || currentUser?.follower?.length === undefined
+    || currentUser?.follower?.length === null) ? 0 : currentUser?.follower?.length;
+
+  useEffect(() => {
+    getCurrentUser().then((res) => {
+      setCurrentUser(res[0]);
+    });
+  }, []);
+  // using state to save image[
+  const [image1, setImage1] = useState('');
+  const [image2, setImage2] = useState('');
+  const [image3, setImage3] = useState('');
+  const [image4, setImage4] = useState('');
+  const [image5, setImage5] = useState('');
+  const [image6, setImage6] = useState('');
+  const [image7, setImage7] = useState('');
+  const [image8, setImage8] = useState('');
+  const [image9, setImage9] = useState('');
+
+  const firstRendering = useRef(true);
+
+  async function loadImage() {
+    if (!firstRendering) {
+      return;
+    }
+    const posts = await getPosts();
+    console.log('posts are:', posts)
+    setImage1(posts[0]);
+    setImage2(posts[1]);
+    setImage3(posts[2]);
+    setImage4(posts[3]);
+    setImage5(posts[4]);
+    setImage6(posts[5]);
+    setImage7(posts[6]);
+    setImage8(posts[7]);
+    setImage9(posts[8]);
+    //console.log('image4', posts[3].content.length)
+    // console.log(image3);
+    firstRendering.current = false;
+  }
+  if (firstRendering.current) {
+    firstRendering.current = false;
+    loadImage();
+  }
+
+  return (
+    <div id="react-root">
+      <div aria-disabled="false" role="button">
+        <Link to="/Feed">
+          <img src={smallLogo} className="sg-logo-small" alt="sg-logo-small" />
+        </Link>
+      </div>
+      <div className="main-body">
+        <div className="profile-bar">
+          <div aria-disabled="false" role="button">
+            <img src={defaultUser} className="default-user" alt="default-user" placeholder={defaultUser} />
+          </div>
+          <span className="profile-username">{currentUser?.username}</span>
+          <span className="posts">Posts</span>
+          <span className="followers">Followers</span>
+          <span className="following">Following</span>
+          <span className="posts-count">{postNum}</span>
+          <Link to="/Find" state={{ listType: 'followers' }}>
+            <span className="followers-count">{followerCount}</span>
+          </Link>
+          <Link to="/Find" state={{ listType: 'following' }}>
+            <span className="following-count">{followingCount}</span>
+          </Link>
+          <div aria-disabled="false" role="button">
+            <Link to="/upload">
+              <img src={circledPlus} className="circled-plus" alt="circled-plus" />
+            </Link>
+          </div>
+          <div aria-disabled="false" role="button">
+            <Link to="/setting">
+              <img src={setting} className="user-setting" alt="user-setting" />
+            </Link>
+          </div>
+        </div>
+        <div className="boxes">
+          <Link to={`/post/${image1?._id}`} state={{ post: image1 }}>
+            <div className="box1">
+              <img src={image1?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image2?._id}`} state={{ post: image2 }}>
+            <div className="box2">
+              <img src={image2?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image3?._id}`} state={{ post: image3 }}>
+            <div className="box3">
+              <img src={image3?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image4?.id}`} state={{ post: image4 }}>
+            <div className="box4">
+              <img src={image4?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image5?.id}`} state={{ post: image5 }}>
+            <div className="box5">
+              <img src={image5?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image6?.id}`} state={{ post: image6 }}>
+            <div className="box6">
+              <img src={image6?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image7?.id}`} state={{ post: image7 }}>
+            <div className="box7">
+              <img src={image7?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image8?.id}`} state={{ post: image8 }}>
+            <div className="box8">
+              <img src={image8?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+          <Link to={`/post/${image9?.id}`} state={{ post: image9 }}>
+            <div className="box9">
+              <img src={image9?.content} alt="" width="100%" height="100%" />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default profile;
