38e3f05b60c282bbc97444e9a11c269a91a2f185
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2023-02-16 02:22:57 -0500

    Initialize xinyueHW4

---
+++ socialgroundapp/src/components/Comment.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'prop-types';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+// import { mentionsInput, Mention } from 'react-mentions';
+
+function Comment({ commentObj, users }) {
+  const { userId } = commentObj;
+  // const { username } = commentObj;
+  const commentUser = users.find(function (element) {
+    console.log('element is: ');
+    console.log(element);
+    console.log(element._id);
+    return element._id === ('' + userId);
+    // return element.username === username;
+  });
+  // (t) => t._id === JSON.stringify(userId));
+  // console.log(users.find);
+  // console.log('' + userId);
+  console.log('This is Comment.js line 16++++++++++++++++++');
+  console.log(commentUser);
+  const displayName = commentUser?.username;
+  const { content } = commentObj;
+  // console.log(commentObj);
+  // console.log(users);
+  // const printingStuff = async () => {
+  //   let i;
+  //   const array = [];
+  //   for(i = 0; i < users.length; i += 1){
+  //     if(users[i]._id === userId)
+  //   }
+  //     )  }
+  return (
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: '36ch',
+        bgcolor: 'Background.paper',
+      }}
+    >
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1609220361638-14ceb45e5e1e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" />
+        </ListItemAvatar>
+        <ListItemText
+          primary={displayName}
+          secondary={(
+            <Typography
+              component="span"
+              variant="body2"
+              color="textPrimary"
+              sx={{
+                display: 'inline',
+              }}
+            >
+              {content}
+            </Typography>
+          )}
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+    </List>
+  );
+}
+Comment.propTypes = {
+  commentObj: PropTypes.shape({
+    userId: PropTypes.string,
+    content: PropTypes.string,
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
+  users: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    username: PropTypes.string.isRequired,
+    email: PropTypes.string,
+    password: PropTypes.string,
+    following: PropTypes.arrayOf(PropTypes.string),
+    followers: PropTypes.arrayOf(PropTypes.string),
+    postCount: PropTypes.number,
+    _id: PropTypes.string.isRequired,
+  })).isRequired,
+};
+export default Comment;
