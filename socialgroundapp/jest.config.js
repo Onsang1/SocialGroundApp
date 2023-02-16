868853db77724b3fd87acc755765254becaf8a00
Onsang1 <113162579+Onsang1@users.noreply.github.com>
2023-02-16 02:23:00 -0500

    Initialize 122122_CL_OY_visibility

---
+++ socialgroundapp/jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-useless-escape */
+module.exports = {
+  transformIgnorePatterns: ['node_modules/(?!\\@?axios)'],
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/cssStub.js',
+    '\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mediaStub.js',
+  },
+  resetMocks: false,
+  setupFiles: ["jest-localstorage-mock"],
+};
